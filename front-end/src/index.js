a9068a54396271c46e53ed42afc1fe08126ef31d
CoreyPickett <pickettcorey23@gmail.com>
2025-05-15 17:56:25 +1000

    Started Implementing Firebase Auth

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -4,6 +4,22 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAP3fJDGdNR_d5WeFGZ7MpQVShzF5I1mvk",
+  authDomain: "uon-community-marketplac-8327c.firebaseapp.com",
+  projectId: "uon-community-marketplac-8327c",
+  storageBucket: "uon-community-marketplac-8327c.firebasestorage.app",
+  messagingSenderId: "465593244377",
+  appId: "1:465593244377:web:8c125eb152218b9dacbcc8",
+  measurementId: "G-0TXYLTCXG8"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 // Used to run app, similar to main.jsx in tutorial video
 createRoot(document.getElementById('root')).render( 
   <StrictMode>
